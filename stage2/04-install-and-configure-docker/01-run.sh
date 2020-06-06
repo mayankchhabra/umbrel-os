cacdfe83f97558c1d2e9bb16196d3555703c5846
Mayank <mayankchhabra9@gmail.com>
2020-06-06 16:57:57 +0530

    Add Bazel APT key and move docker dependencies

--- stage2/04-install-and-configure-docker/01-run.sh
+++ stage2/04-install-and-configure-docker/01-run.sh
@@ -4,6 +4,9 @@ echo "Adding Docker’s official GPG key"
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 
+echo "Add Bazel APT repository key"
+curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+
 echo "Set up the stable repository to download Docker"
 add-apt-repository \
    "deb [arch=armhf] https://download.docker.com/linux/debian \
