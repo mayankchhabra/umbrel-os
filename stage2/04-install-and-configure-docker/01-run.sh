fd04d65fc0ab3ea96c6ec1cf678329417ef1c602
Mayank <mayankchhabra9@gmail.com>
2020-06-06 17:45:11 +0530

    Separate Docker installation and config

--- stage2/04-install-and-configure-docker/01-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-echo "Adding Docker’s official GPG key"
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
-
-echo "Add Bazel APT repository key"
-curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
--- stage2/05-configure-docker/00-run.sh
+++ stage2/05-configure-docker/00-run.sh
@@ -4,4 +4,3 @@ on_chroot << EOF
 usermod -a -G docker $FIRST_USER_NAME
 chown -R $FIRST_USER_NAME:$FIRST_USER_NAME /home/$FIRST_USER_NAME
 EOF
-
