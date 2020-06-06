891b03756377f6703825289e1f71e430544e770e
Mayank <mayankchhabra9@gmail.com>
2020-06-06 17:39:46 +0530

    Install Docker via official script

--- stage2/04-install-and-configure-docker/01-run.sh
+++ stage2/04-install-and-configure-docker/01-run.sh
@@ -6,11 +6,3 @@ apt-key fingerprint 0EBFCD88
 
 echo "Add Bazel APT repository key"
 curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
-
-echo "Set up the stable repository to download Docker"
-add-apt-repository \
-   "deb [arch=armhf] https://download.docker.com/linux/debian \
-   buster \
-   stable"
-
-apt-get update
